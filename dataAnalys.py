217217336fb9a464e39be25bd3f0fc966547404d
abdellatifboukernine <31359561+davidlotfi@users.noreply.github.com>
2019-04-17 23:20:40 +0200

    Add files via upload

--- dataAnalys.py
+++ dataAnalys.py
@@ -8,7 +8,7 @@ print("----------------------------------------")
 c=np.arange(12).reshape(4,3)
 print(c)
 print("----------------------------")
-d=np.full((10,20),4)
+d=np.full((10,20),5)
 
 print(d)
 
@@ -16,3 +16,8 @@ print(f" la dimention de table :{d.ndim}")
 print(f" la taille de table :{d.size}")
 print(f" la form de table :{d.shape}")
 print(f" la type de table :{d.dtype}")
+print(f"element dans l 1er ligne est 6eme colones  {d[0,6]}")
+d[0,6]=7
+d[1,6]=8
+d[2,6]=1
+print(d)
