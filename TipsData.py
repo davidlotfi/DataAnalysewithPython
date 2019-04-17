e3d4614d935e0b7ff1b6a6f3c12d03c9b4339690
abdellatifboukernine <31359561+davidlotfi@users.noreply.github.com>
2019-04-18 01:33:07 +0200

    Add files via upload

---
+++ TipsData.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Initialize Figure and Axes object
+fig, ax = plt.subplots()
+
+# Load in data
+tips = pd.read_csv("https://raw.githubusercontent.com/mwaskom/seaborn-data/master/tips.csv")
+
+# Create violinplot
+ax.violinplot(tips["total_bill"], vert=False)
+
+# Show the plot
+plt.show()
