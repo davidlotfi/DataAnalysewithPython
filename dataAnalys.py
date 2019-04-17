4640c2bd084df5a6b6e18e54e781e54cacb59a63
abdellatifboukernine <31359561+davidlotfi@users.noreply.github.com>
2019-04-17 23:15:13 +0200

    Add files via upload

--- dataAnalys.py
+++ dataAnalys.py
@@ -1,6 +1,18 @@
 import numpy as np
 a=np.array([0,30,45,60,90])
-b=np.array([[0,2,3,4,5,6],
+b=np.array([[0,2,3,4,5,6],  #matrice
            [7,8,9,77,88,99]])
 print(a)
 print(b)
+print("----------------------------------------")
+c=np.arange(12).reshape(4,3)
+print(c)
+print("----------------------------")
+d=np.full((10,20),4)
+
+print(d)
+
+print(f" la dimention de table :{d.ndim}")
+print(f" la taille de table :{d.size}")
+print(f" la form de table :{d.shape}")
+print(f" la type de table :{d.dtype}")
