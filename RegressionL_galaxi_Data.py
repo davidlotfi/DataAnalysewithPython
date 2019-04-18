79dca27b1a0adf47a53b65a96b3380b92eea6976
abdellatifboukernine <31359561+davidlotfi@users.noreply.github.com>
2019-04-19 00:24:52 +0200

    Add files via upload

--- RegressionL_galaxi_Data.py
+++ RegressionL_galaxi_Data.py
@@ -1,12 +1,23 @@
 import matplotlib.pyplot as plt
 import pandas as pd
+from sklearn.linear_model import LinearRegression
+
 
 fig, ax = plt.subplots()
+
 # Load in data
 galaxis = pd.read_csv("hubble_data.csv")
+#print(galaxis.head())
+x=galaxis[[0]]
+y=galaxis[[1]]
+#reg=LinearRegression().fit(galaxis["distance"],galaxis["recession_velocity"])
+reg=LinearRegression().fit(x,y)
 
-# Create violinplot
-ax.violinplot(galaxis["distance"], vert=False)
+plt.scatter(galaxis["distance"],galaxis["recession_velocity"])
+plt.plot(galaxis["distance"],galaxis["recession_velocity"])
+plt.xticks()
+plt.yticks()
+
 
 # Show the plot
 plt.show()
